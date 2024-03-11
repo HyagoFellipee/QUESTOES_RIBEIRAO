4c13c1494a9e5a34b026f8e1b5a31af128c14c62
Hyago Fellipe <hyago.soares@cear.ufpb.br>
2024-03-11 15:58:53 -0300

    adiciona quarta questão

---
+++ 4.py
@@ -0,0 +1,10 @@
+
+# 4) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em uma sala diferente. Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada.
+
+# Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?
+
+# A cada ida, faço o seguinte: ligo um interruptor e espero um tempo. Depois desligo o interruptor e ligo outro. 
+# Entro em qualquer uma das salas e vejo a lâmpada. Se estiver acesa, o interruptor que liguei controla a lampada. 
+# Se estiver apagada e a lâmpada estiver quente, o interruptor que liguei e desliguei controla a lâmpada.
+# Se estiver apagada e a lâmpada estiver fria, o interruptor que não liguei controla a lâmpada.
+# Dessa forma, com duas idas, consigo descobrir qual interruptor controla cada lâmpada.
